fe0a256b60bf16f1ffdaa52ead62f586ac48740a
paul <ojieh.p78@yahoo.com>
2025-08-13 18:29:19 +0100

    worked on product controller last

--- Program.cs
+++ Program.cs
@@ -1,11 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using ProductManagement.Application.Services;
+using ProductManagement.Domain.Repositories;
+using ProductManagement.Infrastructure.Data;
+using ProductManagement.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+          .AddJsonOptions(options =>
+          {
+              //Disable Camel case naming conventions for JSON Serialization and Deserialization
+              options.JsonSerializerOptions.PropertyNamingPolicy = null;
+          });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add DbContext with SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register services and repositories
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
